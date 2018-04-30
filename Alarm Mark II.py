7d2a136dae7972489e7e95745dbe300ab31b0e79
Riley Carpenter <31159790+rileyball@users.noreply.github.com>
2018-04-30 16:13:07 -0400

    Update Alarm Mark II.py

--- Alarm Mark II.py
+++ Alarm Mark II.py
@@ -73,14 +73,12 @@ def alarmsystem(Hours2, Minutes2):
     Hours1 = int(Currenttime[11:13])
     Minutes1 = int(Currenttime[14:16])
     Seconds = int(Currenttime[17:19])
+    soundisplaying = False
     if timeofday == "Bedtime" and (os.path.exists(dir_path +"/Backgroundsounds")) == True:
         soundisplaying = True
         playsound(random.choice(background))
     elif soundsorno == "Y" or soundsorno == "Yes" or soundsorno == "y" or soundsorno == "yes" and (os.path.exists(dir_path + "/Backgroundsounds")) == True:
         playsound(random.choice(background))
-    elif (os.path.exists(dir_path + "/Silence" == True)) and timeofday != "Bedtime" and timeofday != "Night":
-        soundisplaying = True
-        playsound(random.choice(Silence))
     while Hours1 != Hours2 or Minutes1 != Minutes2:
         if secondtime == True:
             print(fiveminutecountdown)
