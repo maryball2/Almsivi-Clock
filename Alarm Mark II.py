934420d49c0b1dd49cbaf1867aab0b8c598a67c1
Riley Carpenter <31159790+rileyball@users.noreply.github.com>
2018-05-05 11:29:01 -0400

    Update Alarm Mark II.py

--- Alarm Mark II.py
+++ Alarm Mark II.py
@@ -1,8 +1,7 @@
-#!/usr/bin/env python
 '''
-Title: This is an alarm that perfectly lines up with my sleep schedule
+Title: Alarm Mark II
 Author: Riley Carpenter
-TODO: have the volume get louder when it goes off and quieter afterwards, if possible have it both run and end redshift when it is nightime
+TODO: have the volume get louder when it goes off and quieter afterwards, if possible have it both run and end redshift when it is nightime, large text for the alarm section would be nice and also make it easier to read in the morning
 '''
 from pygame import mixer
 import time
@@ -13,6 +12,12 @@ import getpass
 import glob
 import os
 global secondtime
+global seconds
+global minutes
+global hours
+global days
+global years
+global decades
 dir_path = os.path.dirname(os.path.realpath(__file__))
 optionalsongs = (glob.glob(dir_path + "/Songs/*.wav*"))
 '''
@@ -41,11 +46,31 @@ Seconds = int(Currenttime[17:19])
 fiveminutecountdown = 300
 startinghour = int(Currenttime[11:13])
 soundisplaying = False
+milliseconds = 0
+seconds = 0
+minutes = 0
+hours = 0
+days = 0
+years = 0
+decades = 0
+class color: #This is for bold apparently??? IDK I found it on stack overflow
+   PURPLE = '\033[95m'
+   CYAN = '\033[96m'
+   DARKCYAN = '\033[36m'
+   BLUE = '\033[94m'
+   GREEN = '\033[92m'
+   YELLOW = '\033[93m'
+   RED = '\033[91m'
+   BOLD = '\033[1m'
+   UNDERLINE = '\033[4m'
+   END = '\033[0m'
+#This is where I make the clearscreen function universal for all systems
 global clearorcls
 if sys.platform == "linux" or sys.platform == "posix":
     clearorcls = "clear"
 else:
     clearorcls = "cls"
+#This is where I make it so it's easier to tell what the time of day is
 if startinghour >= 22 or startinghour <= 4:
     timeofday = "Bedtime"
 elif startinghour >= 6 and startinghour <= 8:
@@ -58,6 +83,7 @@ elif startinghour >= 13 and startinghour <= 18:
     timeofday = "Afternoon"
 elif startinghour >= 17 and startinghour <= 22:
     timeofday = "Night"
+#The phrases that go off during the alarm? Set here
 if timeofday == "Bedtime":
     phrases = ["Wake up Riley!!!!","It's time to wake up it's time to wake up","HEEEEYYY WAKE UP","RILEY RILEY RILEY WAKE UP","1 2 3 4 5 6 7 8 9 it is time to wake up","Riley more alarms are to come UNLESS you get up","OH WHEN SHALL I SEE JESUS you wanna not hear this again? Wake up","I'm so tired of telling you to wake up just wake up","A friend of the devil is somehow who doesn't wake up","Babe babe bae wake up"]
 elif timeofday == "Morning":
@@ -84,7 +110,12 @@ def gettime(): #This gets the current time but I put this in so late I never use
     Minutes1 = int(Currenttime[14:16])
     Seconds = int(Currenttime[17:19])
 def alarmsystem(Hours2, Minutes2): #Main alarm loop
-    alarmtimer = 0
+    seconds = 0
+    minutes = 0
+    hours = 0
+    days = 0
+    years = 0
+    decades = 0
     global soundisplaying
     Currenttime = time.ctime()
     Hours1 = int(Currenttime[11:13])
@@ -118,15 +149,68 @@ def alarmsystem(Hours2, Minutes2): #Main alarm loop
             Minutes2 = str("0" + str(Minutes2))
         else:
             Minutes2 = str(Minutes2)
-        print(Hours1 + ":" + Minutes1,"is not",Hours2 + ":" + Minutes2,"therefore the alarm is not going off")
+        print("\033[1m" + Hours1 + ":" + Minutes1,"is not",Hours2 + ":" + Minutes2,"therefore the alarm is not going off" + "\033[0m")
         print("")
         print("")
         print("")
         print("")
-        print(alarmtimer,"seconds passed")
-        alarmtimer += 1
+        #This is the area that controls the stopwatch that counts how long you've had the alarm on
+        seconds += 1
+        if seconds == 60:
+            seconds = 0
+            minutes +=1
+        elif minutes == 60:
+            minutes = 0
+            hours += 1
+        elif hours == 24:
+            hours = 0
+            days != 1
+        elif days == 365:
+            days = 0
+            years += 1
+        elif years == 10:
+            years = 0
+            decades += 1
+        while decades >= 1:
+            print("You've been running this alarm for a straight decade??? HOw???")
+        if years == 1:
+            year = "year"
+        else:
+            year = "years"
+        if days == 1:
+            day = "day"
+        else:
+            day = "days"
+        if hours == 1:
+            hour = "hour"
+        else:
+            hour = "hours"
+        if minutes == 1:
+            minute = "minute"
+        else:
+            minute = "minutes"
+        if seconds == 1:
+            second = "second"
+        else:
+            second = "seconds"
+        if milliseconds == 1:
+            millisecond = "millisecond"
+        else:
+            millisecond = "milliseconds"
+        if years >= 1:
+            fulltime = ["The amount of time that has passed is",years,year,hours,hour,minutes,minute,seconds,second]
+        elif days >= 1:
+            fulltime = ["The amount of time that has passed is",days,day,hours,hour,minutes,minute,seconds,second]
+        elif hours >= 1:
+            fulltime = ["The amount of time that has passed is",hours,hour,minutes,minute,seconds,second]
+        elif minutes >= 1:
+            fulltime = ["The amount of time that has passed is",minutes,minute,seconds,second]
+        else:
+            fulltime = ["The amount of time that has passed is",seconds,second]
+        print(*fulltime,sep = " ")
+        #End of that area that does the stopwatch
         Hours1 = int(Hours1)
-        Minutes1 = int(Minutes1)
+        Minutes1 = int(Minutes1) #Reset everything to integer from string
         Hours2 = int(Hours2)
         Minutes2 = int(Minutes2)
         time.sleep(1)
@@ -142,7 +226,7 @@ def alarmsystem(Hours2, Minutes2): #Main alarm loop
         endorno = input("Press enter to stop the music and snooze for 5 minutes or type stop ")
         if endorno == "stop":
             if getpass.getuser() == "rileyball2": #My user account, this is so I can set my own nicknames here, if you want to you can modify this
-                name2 = ["Riles","Riley Carpenter","Riley Mitchel Carpenter","Rileybug","Rileyball","Rile","Michelle","Riley Michelle Carpenter","Diane","Comrade"] #Bunch of nickity names for me
+                name2 = ["Riles","Riley Carpenter","Riley Mitchel Carpenter","Rileybug","Rileyball","Rile","Michelle","Riley Michelle Carpenter","Diane","Comrade","Conrad","my really cool nonbinary pal"] #Bunch of nickity names for me
                 name = random.choice(name2)
             else:
                 name = getpass.getuser()
